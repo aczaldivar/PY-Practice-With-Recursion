5ed300c9d19053f53ff87641c4806a50097fc751
Anna Zaldivar <anna.zaldivar@gmail.com>
2023-08-06 08:43:07 -0700

    practice

--- solution1.py
+++ solution1.py
@@ -1,1 +1,12 @@
 # Write code for algorithm 1 below
+def count_down(n):
+  #inherent base case
+  #(will stop if n <= 0)
+  if n>0:
+      #recursive case
+      print(n)
+      count_down(n-1)
+   
+#test case
+n=8
+count_down(n)
--- solution3.py
+++ solution3.py
@@ -1,1 +1,16 @@
 # Write code for algorithm 3 below
+def fibonacci (n):
+    if n<=0:
+        print("incorrect input")
+    elif n==1:
+        return 0
+    elif n==2:
+        return 1
+    else:
+        return fibonacci(n-1)+fibonacci(n-2)    
+print ("2nd fibonacci number:")
+print (fibonacci(2))
+print("4th fib. number")
+print(fibonacci(4))
+print("8th fib. number:")
+print(fibonacci(8))
